105b0812fbe88bd1b27b8f2717963a8c76c6987d
atiyahzahra <atyzhr2@gmail.com>
2024-05-03 09:52:53 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,5 @@ use App\Http\Controllers\BmiController;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\Routing\Annotation\Route as AnnotationRoute;
 
-Route::
 Route::get('/bmi/create', [BMIController::class, 'create'])->name('bmi.create');
 Route::post('/bmi/calculate', [BMIController::class, 'calculate'])->name('bmi.calculate');
