c260438d780ce4c78d0a8b8435cfbe5702af759f
rasalhaguee <rasalhague@student.telkomuniversity.ac.id>
2024-05-09 19:09:56 +0700

    Foodtrack
    ..

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\BmiController;
+use App\Http\Controllers\FoodController;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\Routing\Annotation\Route as AnnotationRoute;
 
 Route::get('/bmi/create', [BMIController::class, 'create'])->name('bmi.create');
 Route::post('/bmi/calculate', [BMIController::class, 'calculate'])->name('bmi.calculate');
+Route::get('/food/create', [FoodController::class, 'create'])->name('food.create');
+Route::post('/food/store', [FoodController::class, 'store'])->name('food.store');
