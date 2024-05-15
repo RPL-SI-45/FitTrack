f058fb1fb0f3557ba9f0d9d3453f38d4adb2dea1
diragarin <indiraagustiagarini@gmail.com>
2024-05-15 19:50:01 +0700

    tes

--- FitTrack/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\BmiController;
-use Illuminate\Support\Facades\Route;
-
-
-Route::get('/bmi/create', [BMIController::class, 'create'])->name('bmi.create');
-Route::post('/bmi/calculate', [BMIController::class, 'calculate'])->name('bmi.calculate');
