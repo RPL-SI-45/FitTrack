bcc717913be76a4bed915003fc62a9ea72fa4edf
atiyahzahra <atyzhr2@gmail.com>
2024-05-25 16:21:09 +0700

    update

--- app/Http/Controllers/BmiController.php
+++ app/Http/Controllers/BmiController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\BmiRecord;
-use App\Models\body_mass_indices;
 use App\Models\BodyMassIndex;
 
 class BmiController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\FoodController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\FoodTrackController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/sesi', [LoginController::class, 'index']);
